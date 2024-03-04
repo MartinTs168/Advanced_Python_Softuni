30f3bffea425fd51b65aec2203bcaa3427ee0cd1
MartinTs168 <martin.tsenev@gmail.com>
2024-03-04 19:28:43 +0200

    Added solutions to 6 new problems.

--- OOP/inheritence_exercise/05.Shop/project/product_repository.py
+++ OOP/inheritence_exercise/05.Shop/project/product_repository.py
@@ -16,11 +16,8 @@ class ProductRepository:
             pass
 
     def remove(self, product_name: str):
-        try:
-            product = next(filter(lambda x: x.name == product_name, self.products))
-            self.products.remove(product)
-        except StopIteration:
-            pass
+        product = self.find(product_name)
+        self.products.remove(product)
 
     def __repr__(self):
         result = "\n".join(f"{p.name}: {p.quantity}" for p in self.products)
